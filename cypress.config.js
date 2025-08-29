dc0140776d152fe5780308557e0324004d52bc88
Donatto293 <donatojimenez293@gmail.com>
2025-08-29 18:53:22 -0500

    ultimo con suerte

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,14 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:5050",
     supportFile: false,
-    specPattern: "cypress/e2e/**/*.js",
+    specPattern: "cypress/e2e/**/*.js"
   },
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "mochawesome-report",
+    overwrite: false,
+    html: true,
+    json: false,
+    timestamp: "mmddyyyy_HHMMss" // Opcional: agrega timestamp para evitar conflictos
+  }
 });
